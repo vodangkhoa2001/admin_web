ac4c193d481365c63c2fdcb2b64e7ad0682399ca
vodangkhoa2001 <vdk0610@gmail.com>
2022-02-20 00:51:35 +0700

    chinh sua TaiKhoanSeeder

--- database/seeders/TaiKhoanSeeder.php
+++ database/seeders/TaiKhoanSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 use App\Models\TaiKhoan;
-use Illuminate\Support\Facades\Hash;
+
 
 class TaiKhoanSeeder extends Seeder
 {
@@ -15,31 +17,45 @@ class TaiKhoanSeeder extends Seeder
      */
     public function run()
     {
-        //
-        $TaiKhoan = new TaiKhoan();
-        $TaiKhoan->id="TAIKHOAN01";
-        $TaiKhoan->TenDangNhap="huy556";
-        $TaiKhoan->MatKhau= Hash::make("123456");
-        $TaiKhoan->Email="huynguyen@gamil.com";
-        $TaiKhoan->SDT="0123541485";
-        $TaiKhoan->DiaChi="TP Hồ Chí Minh";
-        $TaiKhoan->HoTen="Nguyễn Văn A";
-        $TaiKhoan->HinhAnh="user01.png";
-        $TaiKhoan->ID_LoaiTaiKhoan=1;
-        $TaiKhoan->TrangThai_TaiKhoan=1;
-        $TaiKhoan->save();
+        DB::table('taikhoan')->insert([
+            'id'=>'USER01',
+            'TenDangNhap'=>'vdk',
+            'HoTen' => 'Dang Khoa',
+            'Email' => 'vdk@gmail.com',
+            'MatKhau' => Hash::make('password'),
+            'SDT'=>'0909876789',
+            'DiaChi'=>'hcm city',
+            'HinhAnh'=>'user_01.png',
+            'ID_LoaiTaiKhoan'=>1,
+            'TrangThai_TaiKhoan'=>1,
+        ]);
 
-        $TaiKhoan = new TaiKhoan();
-        $TaiKhoan->id="TAIKHOAN02";
-        $TaiKhoan->TenDangNhap="Khoa112";
-        $TaiKhoan->MatKhau= Hash::make("123456");
-        $TaiKhoan->Email="khoanguyen@gamil.com";
-        $TaiKhoan->SDT="0123546485";
-        $TaiKhoan->DiaChi="TP Hồ Chí Minh";
-        $TaiKhoan->HoTen="Nguyễn Văn Khoa";
-        $TaiKhoan->HinhAnh="user1.png";
-        $TaiKhoan->ID_LoaiTaiKhoan=1;
-        $TaiKhoan->TrangThai_TaiKhoan=1;
-        $TaiKhoan->save();
+        //
+        // $TaiKhoan = new TaiKhoan();
+        // $TaiKhoan->id="TAIKHOAN01";
+        // $TaiKhoan->TenDangNhap="huy556";
+        // $TaiKhoan->MatKhau= Hash::make("123456");
+        // $TaiKhoan->Email="huynguyen@gamil.com";
+        // $TaiKhoan->SDT="0123541485";
+        // $TaiKhoan->DiaChi="TP Hồ Chí Minh";
+        // $TaiKhoan->HoTen="Nguyễn Văn A";
+        // $TaiKhoan->HinhAnh="user01.png";
+        // $TaiKhoan->ID_LoaiTaiKhoan=1;
+        // $TaiKhoan->TrangThai_TaiKhoan=1;
+        // $TaiKhoan->save();
+
+        // $TaiKhoan = new TaiKhoan();
+        // $TaiKhoan->id="TAIKHOAN02";
+        // $TaiKhoan->TenDangNhap="Khoa112";
+        // $TaiKhoan->MatKhau= Hash::make("123456");
+        // $TaiKhoan->Email="khoanguyen@gamil.com";
+        // $TaiKhoan->SDT="0123546485";
+        // $TaiKhoan->DiaChi="TP Hồ Chí Minh";
+        // $TaiKhoan->HoTen="Nguyễn Văn Khoa";
+        // $TaiKhoan->HinhAnh="user1.png";
+        // $TaiKhoan->ID_LoaiTaiKhoan=1;
+        // $TaiKhoan->TrangThai_TaiKhoan=1;
+        // $TaiKhoan->save();
+
     }
 }
